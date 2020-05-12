6a1e2a68c4ae4e95e4c882e2cb9593cd970c2af3
vladok089 <vladok.sha089@gmail.com>
2020-05-13 01:33:41 +0200

    implemented reducer for changing book list state

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -6,22 +6,13 @@ import { bookReducer } from '../reducers/bookReducer';
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-
   const [books, dispatch] = useReducer( bookReducer, [
     {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
     {title: 'the final empire', author: 'brandon sanderson', id: 2}
   ]);
 
-  const addBook = (title, author) => {
-    setBooks([...books, {title, author, id: uuid()}]);
-  }
-
-  const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id));
-  }
-
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       { props.children }
     </BookContext.Provider>
   )
